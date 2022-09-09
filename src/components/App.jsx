d3f96311e27216bb68ff1f510483a4c01a63c0fb
Syekh Guntur AP <syekh.guntur10@gmail.com>
2022-09-09 17:39:35 +0700

    Add icon, animation and background

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,37 +5,35 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = useState([]);
 
-    const [notes, setNotes] = useState([]);
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
 
-    function addNote(note) {
-        setNotes(prevNotes => {
-           return [...prevNotes, note];
-        });
-    }
-
-    function deleteNote(id) {
-        setNotes(prevNotes => {
-            return prevNotes.filter((noteItem, index) => {
-                return index !== id;
-            })
-        });
-    }
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
   return (
     <div>
       <Header />
-      <CreateArea 
-        onAdd={addNote}
-      />
+      <CreateArea onAdd={addNote} />
       {notes.map((noteItem, index) => {
-        return ( <Note 
+        return (
+          <Note
             key={index}
             id={index}
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
-        />
+          />
         );
       })}
       <Footer />
